fa9f2d88deafe4463625092077e429a6d412607f
Wimas Harjoko <wimasharjoko@gmail.com>
2024-10-31 17:25:41 +0700

    feat: logout personal info screen

---
+++ components/ButtonLogout.js
@@ -0,0 +1,11 @@
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function ButtonLogout() {
+	return (
+		<View className="w-full mt-3 justify-center">
+			<TouchableOpacity className="flex-row h-14 rounded-[12] bg-[#ED3241] items-center justify-center">
+				<Text className="text-ivory text-lg font-semibold">Logout</Text>
+			</TouchableOpacity>
+		</View>
+	);
+}
--- components/ProfileInfo.js
+++ components/ProfileInfo.js
@@ -1,14 +1,14 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Text, View } from "react-native";
 
-export default function ProfileInfo({label, value}) {
-	return(
-		<View className='bg-white rounded-[12] w-full h-24 items-start justify-center px-5 py-3'>
-			<Text className='text-thistle text-md font-light mb-2'>
-				{label}
-			</Text>
-			<Text className='text-evergreen text-lg font-bold'>
-				{value}
-			</Text>
+export default function ProfileInfo({ label, value, nameIcon }) {
+	return (
+		<View className="bg-white rounded-[12] w-full h-24 items-center px-5 py-3 flex-row gap-5">
+			<Ionicons name={nameIcon} size={20} color="#ECD768" className="justify-center" />
+			<View className='flex-col'> 
+				<Text className="text-thistle text-md font-light mb-2">{label}</Text>
+				<Text className="text-evergreen text-lg font-bold">{value}</Text>
+			</View>
 		</View>
-	)
+	);
 }
