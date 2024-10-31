f258615b52dab46e7351179f72f07f8dd27fa97a
Wimas Harjoko <wimasharjoko@gmail.com>
2024-11-01 01:05:51 +0700

    fix : Profile Screen

--- components/ButtonProfile.js
+++ components/ButtonProfile.js
@@ -1,10 +1,11 @@
-import { TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
-export default function ButtonProfile({ children }) {
+export default function ButtonProfile({ text, children }) {
 	return (
 		<View className="w-full mb-4 justify-center">
-			<TouchableOpacity className="flex-row h-14 rounded-full bg-soil items-start py-3 px-6 space-x-3">
+			<TouchableOpacity className="flex-row py-[14] rounded-full bg-soil items-center px-6 gap-3">
 				{children}
+				<Text className="text-ivory text-lg font-semibold">{text}</Text>
 			</TouchableOpacity>
 		</View>
 	);
--- screen/ProfileScreen.js
+++ screen/ProfileScreen.js
@@ -8,25 +8,20 @@ export default function ProfileScreen() {
 		<View className="flex-1 items-center bg-hex-#F8F8F8">
 			<ContainerProfile />
 			<View className="mt-4 mb-3 px-6 py-6 w-full flex-1 bg-white rounded-[30px] items-center">
-				<ButtonProfile>
-					<Ionicons name="person-outline" size={18} className='mr-4 mt-1' color={"#F8F8F8"}/>
-					<Text className="text-ivory text-lg font-semibold">Informasi Profile</Text>
+				<ButtonProfile text={"Informasi Profile"}>
+					<Ionicons name="person-outline" size={18} color={"#F8F8F8"}/>
 				</ButtonProfile>
-				<ButtonProfile>
-					<Ionicons name="key-outline" size={18} className='mr-4 mt-1' color={"#F8F8F8"}/>
-					<Text className="text-ivory text-lg font-semibold">Ubah Password</Text>
+				<ButtonProfile text={"Ubah Password"}>
+					<Ionicons name="key-outline" size={18} color={"#F8F8F8"}/>
 				</ButtonProfile>
-				<ButtonProfile>
-					<Ionicons name="reorder-four-outline" size={18} className='mr-4 mt-1' color={"#F8F8F8"}/>
-					<Text className="text-ivory text-lg font-semibold">Tata Cara Pemesanan</Text>
+				<ButtonProfile text={"Tata Cara Pemesanan"}>
+					<Ionicons name="reorder-four-outline" size={18} color={"#F8F8F8"}/>
 				</ButtonProfile>
-				<ButtonProfile>
-					<Ionicons name="heart-outline" size={18} className='mr-4 mt-1' color={"#F8F8F8"}/>
-					<Text className="text-ivory text-lg font-semibold">Wishlist Gunung</Text>
+				<ButtonProfile text={"Wishlist Gunung"}>
+					<Ionicons name="heart-outline" size={18} color={"#F8F8F8"}/>
 				</ButtonProfile>
-				<ButtonProfile>
-					<AntDesign name="exclamationcircleo" size={18} className='mr-4 mt-1' color={"#F8F8F8"}/>
-					<Text className="text-ivory text-lg font-semibold">Tentang Aplikasi</Text>
+				<ButtonProfile text={"Tentang Aplikasi"}>
+					<AntDesign name="exclamationcircleo" size={18} color={"#F8F8F8"}/>
 				</ButtonProfile>
 			</View>
 		</View>
