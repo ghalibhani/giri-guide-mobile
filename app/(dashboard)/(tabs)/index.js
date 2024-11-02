45c0e79c32a32f1ba29ae639e2d711da10d201c7
ghalib pratama <ghalibpratama.gpro@gmail.com>
2024-11-02 21:44:04 +0700

    refactor

--- app/(dashboard)/(tabs)/index.js
+++ app/(dashboard)/(tabs)/index.js
@@ -1,126 +1,144 @@
-import {
-  StatusBar,
-  StyleSheet,
-  Text,
-  Image,
-  View,
-  ScrollView,
-} from "react-native";
-import React from "react";
-import { useState } from "react";
+import { FlatList, StatusBar, View, Text } from "react-native";
+import OverFlowCarousel from "../../../components/OverFlowCarousel";
+import HeaderHome from "../../../components/HeaderHome";
+import SlideCarousel from "../../../components/SlideCarousel";
 import { SafeAreaView } from "react-native-safe-area-context";
-import TransaksiSlideBerlangsung from "../../../components/transaksiCustomer/TransaksiSlideBerlangsung";
-import TransactionHeader from "../../../components/transaksiCustomer/TransactionHeader";
-import TransactionStatusBar from "../../../components/transaksiCustomer/TransactionStatusBar";
-import CustomButton from "../../../components/miniComponent/CustomButton";
-import TourGuideCard from "../../../components/transaksiCustomer/TourGuideCard";
-import TransactionBerlangsung from "../../../components/transaksiCustomer/TransaksiBerlangsung";
-import TransaksiSelesai from "../../../components/transaksiCustomer/TransaksiSelesai";
+import { useState } from "react";
+import { Link } from "expo-router";
 
-//  transCustomer.js ada di folder transaction
-
-const TransactionCustomerScreen = () => {
-  const tourGuideData = [
+const HomeScreen = () => {
+  const data = [
     {
-      id: "1",
-      guideName: "Johan",
-      mountainName: "Gunung Bromo",
-      hikingPoint: "Pos Paltuding",
-      numHikers: 3,
-      numPorters: 2,
-      numDays: 2,
-      dateRange: "05-06-2023 s/d 07-06-2023",
-      price: "2.500.000",
-      status: "UPCOMING",
-      imageUrl:
-        "https://t4.ftcdn.net/jpg/06/08/55/73/360_F_608557356_ELcD2pwQO9pduTRL30umabzgJoQn5fnd.jpg",
+      id: 1,
+      text: "Item 1",
+      image:
+        "https://img.freepik.com/free-vector/mountains-landscape-sundown_52683-24164.jpg?t=st=1730344884~exp=1730348484~hmac=19b8de13712b886d72c74b1e599df67156e185888242ecc83453e27b1a6d34f4&w=1380",
+      description: "Description 1",
     },
     {
-      id: "2",
-      guideName: "Siti",
-      mountainName: "Gunung Semeru",
-      hikingPoint: "Pos Ranu Pane",
-      numHikers: 5,
-      numPorters: 3,
-      numDays: 3,
-      dateRange: "10-06-2023 s/d 12-06-2023",
-      price: "3.200.000",
-      status: "WAITING_PAY",
-      imageUrl:
-        "https://www.perfocal.com/blog/content/images/2021/01/Perfocal_17-11-2019_TYWFAQ_100_standard-3.jpg",
+      id: 2,
+      text: "Item 2",
+      image:
+        "https://img.freepik.com/free-vector/mountains-landscape-sundown_52683-24164.jpg?t=st=1730344884~exp=1730348484~hmac=19b8de13712b886d72c74b1e599df67156e185888242ecc83453e27b1a6d34f4&w=1380",
+      description: "Description 2",
     },
     {
-      id: "3",
-      guideName: "Siti",
-      mountainName: "Gunung Semeru",
-      hikingPoint: "Pos Ranu Pane",
-      numHikers: 5,
-      numPorters: 3,
-      numDays: 3,
-      dateRange: "10-06-2023 s/d 12-06-2023",
-      price: "3.200.000",
-      status: "WAITING_APPROVE",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/11/03/20/11/portrait-4599553_1280.jpg",
+      id: 3,
+      text: "Item 3",
+      image:
+        "https://img.freepik.com/free-vector/mountains-landscape-sundown_52683-24164.jpg?t=st=1730344884~exp=1730348484~hmac=19b8de13712b886d72c74b1e599df67156e185888242ecc83453e27b1a6d34f4&w=1380",
+      description: "Description 3",
     },
     {
-      id: "4",
-      guideName: "Budi",
-      mountainName: "Gunung Bromo",
-      hikingPoint: "Pos Paltuding",
-      numHikers: 3,
-      numPorters: 2,
-      numDays: 2,
-      dateRange: "05-06-2023 s/d 07-06-2023",
-      price: "2.500.000",
-      status: "DONE",
-      rating: 3,
-      imageUrl:
-        "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+      id: 4,
+      text: "Item 4",
+      image:
+        "https://img.freepik.com/free-vector/mountains-landscape-sundown_52683-24164.jpg?t=st=1730344884~exp=1730348484~hmac=19b8de13712b886d72c74b1e599df67156e185888242ecc83453e27b1a6d34f4&w=1380",
+      description: "Description 4",
     },
     {
-      id: "5",
-      guideName: "Dodo",
-      mountainName: "Gunung Bromo",
-      hikingPoint: "Pos Paltuding",
-      numHikers: 3,
-      numPorters: 2,
-      numDays: 2,
-      dateRange: "05-06-2023 s/d 07-06-2023",
-      price: "2.500.000",
-      status: "REJECT",
-      imageUrl:
-        "https://marketplace.canva.com/EAF21qlw744/1/0/1600w/canva-blue-modern-facebook-profile-picture-mtu4sNVuKIU.jpg",
+      id: 5,
+      text: "Item 5",
+      image:
+        "https://img.freepik.com/free-vector/mountains-landscape-sundown_52683-24164.jpg?t=st=1730344884~exp=1730348484~hmac=19b8de13712b886d72c74b1e599df67156e185888242ecc83453e27b1a6d34f4&w=1380",
+      description: "Description 5",
+    },
+    {
+      id: 6,
+      text: "Item 6",
+      image:
+        "https://img.freepik.com/free-vector/mountains-landscape-sundown_52683-24164.jpg?t=st=1730344884~exp=1730348484~hmac=19b8de13712b886d72c74b1e599df67156e185888242ecc83453e27b1a6d34f4&w=1380",
+      description: "Description 6",
+    },
+    {
+      id: 7,
+      text: "Item 7",
+      image:
+        "https://img.freepik.com/free-vector/mountains-landscape-sundown_52683-24164.jpg?t=st=1730344884~exp=1730348484~hmac=19b8de13712b886d72c74b1e599df67156e185888242ecc83453e27b1a6d34f4&w=1380",
+      description: "Description 7",
+    },
+    {
+      id: 8,
+      text: "Item 8",
+      image:
+        "https://img.freepik.com/free-vector/mountains-landscape-sundown_52683-24164.jpg?t=st=1730344884~exp=1730348484~hmac=19b8de13712b886d72c74b1e599df67156e185888242ecc83453e27b1a6d34f4&w=1380",
+      description: "Description 8",
+    },
+    {
+      id: 9,
+      text: "Item 9",
+      image:
+        "https://img.freepik.com/free-vector/mountains-landscape-sundown_52683-24164.jpg?t=st=1730344884~exp=1730348484~hmac=19b8de13712b886d72c74b1e599df67156e185888242ecc83453e27b1a6d34f4&w=1380",
+      description: "Description 9",
+    },
+    {
+      id: 10,
+      text: "Item 10",
+      image:
+        "https://img.freepik.com/free-vector/mountains-landscape-sundown_52683-24164.jpg?t=st=1730344884~exp=1730348484~hmac=19b8de13712b886d72c74b1e599df67156e185888242ecc83453e27b1a6d34f4&w=1380",
+      description: "Description 10",
     },
   ];
 
-  const [show, setShow] = useState("berlangsung");
+  try {
+    const [hidden, setHidden] = useState(false);
+    const [statusBarStyle, setStatusBarStyle] = useState("default");
+    const [statusBarTransition, setStatusBarTransition] = useState("fade");
 
-  const selesai = ["DONE", "REJECT"];
-  const berlangsung = ["UPCOMING", "WAITING_PAY", "WAITING_APPROVE"];
+    const handleScroll = (event) => {
+      const { nativeEvent } = event;
+      const { contentOffset } = nativeEvent;
+      const { y } = contentOffset;
+      if (y > 300) {
+        setHidden(true);
+      } else {
+        setHidden(false);
+      }
+    };
 
-  const filteredData = tourGuideData.filter((tourGuide) =>
-    show === "berlangsung"
-      ? berlangsung.includes(tourGuide.status)
-      : selesai.includes(tourGuide.status)
-  );
-
-  return (
-    <SafeAreaView className='flex-1'>
-      <StatusBar backgroundColor={"#503A3A"} barStyle={"light-content"} />
-      <View className='bg-grayCustom flex-1 gap-6'>
-        <View className='gap-5'>
-          <TransactionHeader titleHeader='Daftar Transaksi' />
-          <TransactionStatusBar onStatusChange={setShow} />
-          {show === "berlangsung" ? (
-            <TransactionBerlangsung tourGuideData={filteredData} />
-          ) : (
-            <TransaksiSelesai tourGuideData={filteredData} />
+    return (
+      <SafeAreaView>
+        <StatusBar
+          animated={true}
+          backgroundColor='#503a3a'
+          barStyle={statusBarStyle}
+          showHideTransition={statusBarTransition}
+          hidden={hidden}
+          style='light'
+        />
+        <FlatList
+          data={[1]}
+          className='bg-[#f8f8f8]'
+          renderItem={() => (
+            <View>
+              <HeaderHome />
+              <Link href='/transaction/transCustomer'>
+                <Text className='text-3xl font-bold'>loncat</Text>
+              </Link>
+              <SlideCarousel data={data} />
+              <OverFlowCarousel
+                data={data}
+                title={"Jelajahi Gunung di Jawa Timur"}
+              />
+              <OverFlowCarousel
+                data={data}
+                title={"Rute Perjalanan ke Destinasi"}
+                withDescription={true}
+              />
+              <OverFlowCarousel
+                data={data}
+                title={"Pengalaman pendaki lain"}
+                withDescription={true}
+              />
+            </View>
           )}
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+          onScroll={handleScroll}
+        />
+      </SafeAreaView>
+    );
+  } catch (error) {
+    console.error("Error rendering HomeScreen:", error);
+  }
 };
 
-export default TransactionCustomerScreen;
+export default HomeScreen;
