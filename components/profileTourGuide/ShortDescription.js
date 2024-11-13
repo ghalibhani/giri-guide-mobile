c7d945700bd75f947a8ad183d4805667d9a47e8d
ghalib pratama <ghalibpratama.gpro@gmail.com>
2024-11-13 21:50:02 +0700

    minor fix on deskripsi singkat komponen

--- components/profileTourGuide/ShortDescription.js
+++ components/profileTourGuide/ShortDescription.js
@@ -7,7 +7,7 @@ const ShortDescription = ({ data }) => {
     <View className='mx-6 px-6 py-6 rounded-verylarge bg-white gap-3'>
       <View className='flex-row justify-between'>
         <Text className='font-ibold text-soil'>Deskripsi Singkat</Text>
-        <MaterialCommunityIcons name='pencil' color={"#503A3A"} size={20} />
+        {/* <MaterialCommunityIcons name='pencil' color={"#503A3A"} size={20} /> */}
       </View>
 
       <Text className='font-iregular text-evergreen text-sm flex-shrink'>
